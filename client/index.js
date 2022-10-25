20a0249c7feb1155a7bfa3f3add6edd1051d1907
Nehemiah Washington <nehemiah.washington6@gmail.com>
2022-10-25 05:58:01 -0500

    added backend folder

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const PORT = process.env.port || 8080;
 const app = express();
 
+
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public/assets')));
 
